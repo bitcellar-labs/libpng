f7a1f49a942b2bdc116dd7caf8d3262f43966297
shunirr <shunirr@gmail.com>
2012-07-03 14:16:15 +0900

    export c includes

--- Android.mk
+++ Android.mk
@@ -6,5 +6,7 @@ LOCAL_MODULE := libpng
 LOCAL_LDLIBS := -lz
 LOCAL_SRC_FILES := png.c pngerror.c pngget.c pngmem.c pngpread.c pngread.c pngrio.c pngrtran.c pngrutil.c pngset.c pngtrans.c pngwio.c pngwrite.c pngwtran.c pngwutil.c
 
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+
 include $(BUILD_STATIC_LIBRARY)
 
